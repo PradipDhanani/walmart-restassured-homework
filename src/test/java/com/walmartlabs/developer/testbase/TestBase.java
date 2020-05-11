b3bddcad252ce5b5237b448138f37b39f04d70eb
PradipDhanani <pradip.dhanani@gmail.com>
2020-05-11 14:36:24 +0100

    walmart-restassure-homework

---
+++ src/test/java/com/walmartlabs/developer/testbase/TestBase.java
@@ -0,0 +1,20 @@
+package com.walmartlabs.developer.testbase;
+
+import io.restassured.RestAssured;
+import org.junit.BeforeClass;
+
+/**
+ * Created by Pradip
+ */
+public class TestBase {
+
+    public static final String API_KEY = "75e3u4sgb2khg673cbv2gjup";
+
+    @BeforeClass
+    public static void init() {
+        RestAssured.baseURI = "http://api.walmartlabs.com";
+        RestAssured.basePath = "/v1";
+
+
+    }
+}
